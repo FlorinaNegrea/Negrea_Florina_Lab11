36f60060346d4f462b8e868ddc7960f93f1fee45
Negrea Florina <negreaflorina366@yahoo.com>
2023-12-15 21:10:01 +0200

    Add project files.

---
+++ Negrea_Florina_Lab11/ListPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace Negrea_Florina_Lab11;
+using Negrea_Florina_Lab11.Models;
+public partial class ListPage : ContentPage
+{
+	public ListPage()
+	{
+		InitializeComponent();
+	}
+    async void OnSaveButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        slist.Date = DateTime.UtcNow;
+        await App.Database.SaveShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+    async void OnDeleteButtonClicked(object sender, EventArgs e)
+    {
+        var slist = (ShopList)BindingContext;
+        await App.Database.DeleteShopListAsync(slist);
+        await Navigation.PopAsync();
+    }
+
+}
